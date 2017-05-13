51e95cf2f8949b21ae5b2e2e591eca49868de267
Bogdan Orzea <bogdan.orzea@gmail.com>
2017-05-13 19:32:05 +0300

    Adds color for all available activities

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,11 +1,13 @@
 package com.example.android.miwok;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,10 +15,12 @@ import java.util.ArrayList;
 public class WordAdapter extends ArrayAdapter<Word> {
 
     ArrayList<Word> mWord;
+    int mColorResourceId;
 
-    public WordAdapter(Context context, ArrayList<Word> objects) {
+    public WordAdapter(Context context, ArrayList<Word> objects, int colorResourceId) {
         super(context, 0, objects);
         mWord = objects;
+        mColorResourceId = colorResourceId;
     }
 
     @Override
@@ -46,6 +50,10 @@ public class WordAdapter extends ArrayAdapter<Word> {
         } else {
             wordImageView.setVisibility(View.GONE);
         }
+
+        // Set layout background
+        LinearLayout lay = (LinearLayout) listItemView.findViewById(R.id.word_layout);
+        lay.setBackgroundColor(ContextCompat.getColor(getContext(), mColorResourceId));
 
         // Return the view
         return listItemView;
